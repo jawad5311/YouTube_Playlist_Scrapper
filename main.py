b275e2af9d1d51dc5207b57bd867eef271822ef1
Jawad <muhammadjawad5311@gmail.com>
2021-09-27 22:36:24 +0500

    main.py created, YouTube class created and initialized

---
+++ main.py
@@ -0,0 +1,19 @@
+from Google import Create_Service
+
+
+# Creating YouTube class to communicate with YouTube API
+class YouTube:
+    def __init__(self, secret_file, scopes: list=None):
+        self.secret_file = secret_file
+        self.scopes = scopes
+
+    
+
+
+
+
+
+
+
+
+
