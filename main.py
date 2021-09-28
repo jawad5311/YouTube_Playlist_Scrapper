d92b3b459b020858cb51eeab85dcfe6d70b19af5
Jawad <muhammadjawad5311@gmail.com>
2021-09-28 12:34:08 +0500

    videos id retrieved successfully

--- main.py
+++ main.py
@@ -11,20 +11,20 @@ dotenv.load_dotenv()
 # Creating YouTube class to communicate with YouTube API
 class YouTube:
     def __init__(self, key, scopes: list = None):
-        #self.secret_file = secret_file
+        # self.secret_file = secret_file
         self.key = key
         self.scopes = scopes
 
-    def construct_service(self):
-        """
-            Responsible for creating service instance from 'google.Create_Service'
-        """
-        API_SERVICE = 'youtube'
-        API_VERSION = 'v3'
-        service = Create_Service(self.secret_file, API_SERVICE, API_VERSION, self.scopes)
-        return service
+    # def construct_service(self):
+    #     """
+    #         Responsible for creating service instance from 'google.Create_Service'
+    #     """
+    #     API_SERVICE = 'youtube'
+    #     API_VERSION = 'v3'
+    #     service = Create_Service(self.secret_file, API_SERVICE, API_VERSION, self.scopes)
+    #     return service
 
-    def construct_service_2(self):
+    def construct_service(self):
         """
         Creates service object from build method
         """
@@ -63,12 +63,65 @@ class YouTube:
 
         return playlist_id
 
+    @staticmethod
+    def get_playlist_items(service, playlist_Id: str):
+        request = service.playlistItems().list(
+            part='contentDetails',
+            playlistId=playlist_Id,
+            maxResults=10
+        )
+
+        response = request.execute()
+
+        playlist_items = response['items']
+        nextPageToken = response['nextPageToken']
+
+        print(nextPageToken)
+
+        current_page = 1
+
+        while nextPageToken:
+            request = service.playlistItems().list(
+                part='contentDetails',
+                playlistId=playlist_Id,
+                maxResults=10,
+                pageToken=nextPageToken
+            )
+
+            response = request.execute()
+
+            print(f'Current Page: {current_page}')
+            current_page += 1
+
+            playlist_items.extend(response['items'])
+            nextPageToken = response.get('nextPageToken')
+            print(nextPageToken)
+
+        videos_id = []
+
+        for video_id in playlist_items:
+            try:
+                id = video_id['snippet']['resourceId']['videoId']
+                videos_id.append(id)
+
+            except KeyError:
+                id = video_id['contentDetails']['videoId']
+                videos_id.append(id)
+
+        return videos_id
+
+
 
 if __name__ == '__main__':
     API_KEY = os.environ.get('API_KEY')
     yt = YouTube(API_KEY)
-    service = yt.construct_service_2()
-    response = yt.upload_response(service, 'UCSJBJ3sP5GRUJMON12v28ew')
-    print(response)
+    service = yt.construct_service()
+    playlist_id = yt.upload_response(service, 'UCSJBJ3sP5GRUJMON12v28ew')
+    print(playlist_id)
+
+    videos = yt.get_playlist_items(service, playlist_id)
+    print(len(videos))
+
+
 
 
